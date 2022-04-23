49c6133041d4e87a20e3383ff7207192aa8ec0b8
Gabriel Berthier <gnberthier@gmail.com>
2022-04-23 16:31:53 -0300

    fix: set variables to use VUE prefix

--- src/services/aws/index.js
+++ src/services/aws/index.js
@@ -1,12 +1,12 @@
 import AWS from "aws-sdk";
 
 // Inicializar o provedor de credenciais do Amazon Cognito
-AWS.config.region = process.env.AWS_REGION; // Região
+AWS.config.region = process.env.VUE_AWS_REGION; // Região
 AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-  IdentityPoolId: process.env.AWS_INDENTITY_POOL,
+  IdentityPoolId: process.env.VUE_AWS_INDENTITY_POOL,
 });
 
 export const s3 = new AWS.S3({
   apiVersion: "2006-03-01",
-  params: { Bucket: process.env.AWS_BUCKET },
+  params: { Bucket: process.env.VUE_AWS_BUCKET },
 });
